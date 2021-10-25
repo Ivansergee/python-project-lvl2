c91e6b8f175724e6b0c04c24fb8926aa7722d90b
ivan klimentev <klimentievis@inbox.ru>
2021-10-26 01:45:52 +0300

    123

--- gendiff/gendiff/scripts/gendiff.py
+++ gendiff/gendiff/scripts/gendiff.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import argparse
+import pathlib
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=pathlib.Path)
+    parser.add_argument('second_file', type=pathlib.Path)
     parser.add_argument('-f', '--format', help='set the format of output')
 
-    parser.parse_args()
-
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == '__main__':
     main()
