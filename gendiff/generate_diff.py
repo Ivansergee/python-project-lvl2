562221b5f45fbf69769aab58e63639a41d5c5b6c
ivan klimentev <klimentievis@inbox.ru>
2021-11-23 10:34:34 +0000

    fixing

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,8 +1,8 @@
 from gendiff import parse, make_diff, to_string, plain, to_json
 
 
-def generate_diff(file1, file2, format='str'):
-    if format == 'str':
+def generate_diff(file1, file2, format='stylish'):
+    if format == 'stylish':
         return to_string(make_diff(parse(file1), parse(file2)))
     elif format == 'plain':
         return plain(make_diff(parse(file1), parse(file2)))
