63164b7f51f6a915dae104a0c96b9721ff255798
ivan klimentev <klimentievis@inbox.ru>
2021-11-23 12:56:34 +0000

    keep fixing

--- gendiff/formatting/plain.py
+++ gendiff/formatting/plain.py
@@ -4,33 +4,33 @@ from collections import Counter
 def plain(diff, route=''):  # noqa
     diff.sort(key=lambda x: (x['name'], x['prefix']))
     count = Counter(i['name'] for i in diff)
-    res = ''
+    res = []
     for index, node in enumerate(diff):
         name = node['name']
         prefix = node['prefix']
         if 'children' in node:
             new_route = route + name + '.'
-            res += plain(node['children'], new_route)
+            res.append(plain(node['children'], new_route))
         elif prefix == '+':
             if count[name] == 2:
-                res += "Property '{}' was updated. From {} to {}\n".format(
+                res.append("Property '{}' was updated. From {} to {}".format(
                     route + name,
                     check_value(diff[index + 1]['value']),
                     check_value(node['value'])
-                )
+                ))
             else:
-                res += "Property '{}' was added with value: {}\n".format(
+                res.append("Property '{}' was added with value: {}".format(
                     route + name,
                     check_value(node['value'])
-                )
+                ))
         elif prefix == '-':
             if count[name] == 2:
                 continue
             else:
-                res += "Property '{}' was removed\n".format(
+                res.append("Property '{}' was removed".format(
                     route + name
-                )
-    return res
+                ))
+    return '\n'.join(res)
 
 
 def check_value(value):
